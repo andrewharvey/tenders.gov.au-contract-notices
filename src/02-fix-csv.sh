3f167c06233612dfa4a5cfab7a9bdcf24debbf92
Andrew Harvey <andrew.harvey4@gmail.com>
2015-03-19 11:42:22 +1100

    dos2unix tsv files

--- src/02-fix-csv.sh
+++ src/02-fix-csv.sh
@@ -8,6 +8,7 @@ cp -R CNWeeklyExport CNWeeklyExport_TSV
 for f in CNWeeklyExport_TSV/*; do
     cat "$f" | tail -n +3 | sed 's/^\=\"/"/' | sed 's/\t="/\t"/g' > "$tmp"
     cp "$tmp" "$f"
+    dos2unix "$f"
 done
 
 rm -f $tmp
